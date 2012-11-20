d8d79bcb297e7f46d8eca9a90b6272db3168f458
dshartnett <dshartnett@gmail.com>
2012-11-19 19:18:28 -0600

    removed p1

--- project2/backend/server.js
+++
@@ -1,9 +0,0 @@
-//var sys = require('sys');
-var http = require('http');
-var port = 8080;
-http.createServer(function (req, res) {
-	//sys.puts("I got kicked");
-  res.writeHeader(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(port);
-//console.log('Server running on port ' + port);
